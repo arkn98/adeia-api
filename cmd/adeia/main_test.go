ceb3ff2ee9245157aa218bd1700452da57107f7f
arkn98 <20590666+arkn98@users.noreply.github.com>
2020-09-26 23:04:20 +0530

    Fix failing getEnv tests

--- cmd/adeia/main_test.go
+++ cmd/adeia/main_test.go
@@ -10,17 +10,23 @@ import (
 func TestGetEnv(t *testing.T) {
 	t.Run("return fallback if key is not set", func(t *testing.T) {
 		t.Parallel()
+
 		want := "bar"
 		key := "DUMMY_KEY"
+		_ = os.Unsetenv(key)
 		got := getEnv(key, "bar")
 		assert.Equal(t, want, got)
 	})
 
 	t.Run("return value from env if key is set", func(t *testing.T) {
 		t.Parallel()
+
 		key := "DUMMY_KEY"
 		want := "foo"
 		_ = os.Setenv(key, want)
+		defer func() {
+			_ = os.Unsetenv(key)
+		}()
 		got := getEnv("DUMMY_KEY", "bar")
 		assert.Equal(t, want, got)
 	})
